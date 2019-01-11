8d654028cf6c01aa994ac5c5061b9b70bcd2989e
njefferis1 <46577146+njefferis1@users.noreply.github.com>
2019-01-11 13:50:49 -0700

    Start OOPs review solution
    create OOPs review project
    add die class

---
+++ CSReviewSolution/OOPsReview/Die.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsReview
+{
+    public class Die
+    {
+        //data members
+        // this is the physical storage area for data values
+        // these are usually private
+
+        private int _Sides;
+        private string _Color;
+        private int _FaceValue;
+
+        //Properties
+        // properties are public 
+        // is used as an interface for the class user to access a data member
+        // accessing a data member can include a get and set
+        // a get returns the value of the data member to the user
+        // a set accepts a value and assigns the value to the data member
+        // a property returns a specific datatype
+        // a property does NOT have a parameter list
+
+        //Fully implemented property
+        public int Side
+        {
+            get
+            {
+                //returns the curretn value of the data member
+                return _Sides;
+            }
+            set
+            {
+                //can be private
+                //it accepts a value and assigns it to the data member
+                //the value is stored in a key word: value
+                //the datatype of value is the return datatype of the property
+                //validation can be done on the value
+                if(value >= 6 && value <= 20)
+                {
+                    _Sides = value;
+                }
+                else
+                {
+                    throw new Exception("Die cannot have " + value.ToString() + "sides. Die can have 6 to 20 sides.");
+                }
+                
+            }
+        }
+
+
+        //Auto implemented property
+    }
+}
