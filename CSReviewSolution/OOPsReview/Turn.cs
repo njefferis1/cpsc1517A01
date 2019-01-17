0d33b5a5057078236a70038aad26254c2b9e4cee
njefferis1 <46577146+njefferis1@users.noreply.github.com>
2019-01-17 14:46:21 -0700

    Imlemented Turn class

--- CSReviewSolution/OOPsReview/Turn.cs
+++ CSReviewSolution/OOPsReview/Turn.cs
@@ -6,29 +6,20 @@ using System.Threading.Tasks;
 
 namespace OOPsReview
 {
-    class Turn
+    public class Turn
     {
-        public static void TurnOne()
+        public int Player1 { get; set; }
+        public int Player2 { get; set; }
+
+        public Turn()
         {
-            Die diceOne = new Die();
-            Die diceTwo = new Die();
 
-            int p1 = diceOne.FaceValue;
-            int p2 = diceTwo.FaceValue;
-
-            Console.WriteLine("player one score is {0}. player two score is {1}", p1, p2);
         }
 
-
-        public static void AdvTurn()
+        public Turn(int player1, int player2)
         {
-            Die diceOne = new Die(20, "Black");
-            Die diceTwo = new Die(20, "Black");
-
-            int p1 = diceOne.FaceValue;
-            int p2 = diceTwo.FaceValue;
-
-            Console.WriteLine("player one score is {0}. player two score is {1}", p1, p2);
+            Player1 = player1;
+            Player2 = player2;
         }
     }
 }
